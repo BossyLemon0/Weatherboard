be1c1121e4c6a8460440193d9b3001ddad54fb79
Omar D. Ramirez <omardramirez2002@gmail.com>
2021-10-08 16:32:33 -0500

    This commit adds the rest of the numbers onto their respective divs and concatonates them with their labels

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 var requestURl = 'https://api.openweathermap.org/data/2.5/onecall?lat=29.74&lon=-95.46&units=imperial&exclude=minutely,hourly&appid=d878f5584537405a3385fd42ab6df681'
 var eztest = document.querySelectorAll(".head");
 var List = document.querySelectorAll("ul");
+var List2 = document.querySelectorAll(".listal")
 var DateTime = luxon.DateTime;
 var HUMIDITY = []
 var DAYS1 = []
@@ -43,6 +44,7 @@ function getApi(requestUrl) {
 
 function DAYCHOPTOHEAD() {
 
+
 console.log(DAYS1[1]);
   for (let index = 0; index < DAYS1.length-2; index++) {
     var int = parseInt(DAYS1[index], 10)
@@ -55,13 +57,51 @@ console.log(DAYS1[1]);
 function displayinfo() {
 
   console.log(dayinfo);
-var hum = dayinfo[0][0];
+// var hum = dayinfo[0][0];
 // var hum2 = hum[0];
-console.log(hum);
+// console.log(hum);
 
   for (let L = 0; L < 4; L++) {
       for (let i = 0; i < DAYS1.length-2; i++) {
         console.log(dayinfo[L][i]);
+
+        if(L==0){
+          var hum = document.createElement('p');
+          hum.classList.add("listy")
+          hum.textContent = ("Humidity: " + dayinfo[L][i] + "");
+          List2[i].appendChild(hum);
+        }
+
+        if (L==1){
+          var temp = document.createElement('p');
+          temp.classList.add("listy")
+          temp.textContent = ("Temp: " + dayinfo[L][i] + "°");
+          List2[i].appendChild(temp);
+          
+        }
+
+        if(L==2){
+          var windspeed = document.createElement('p');
+          windspeed.classList.add("listy")
+          windspeed.textContent = ("wind: " + dayinfo[L][i] + "");
+          List2[i].appendChild(windspeed);
+        }
+
+        if(L==3){
+          if(i == 0){
+            var uvindex = document.createElement('div');
+          uvindex.classList.add("UV")
+          uvindex.textContent = ("UVI: " + dayinfo[L][i]);
+          List2[i].appendChild(uvindex);
+          }
+          else{
+          var uvindex = document.createElement('p');
+          uvindex.classList.add("listy")
+          uvindex.textContent = ("UVI: " + dayinfo[L][i] + "");
+          List2[i].appendChild(uvindex);
+          }
+        }
+
       // while (L == 0){
       //   console.log("hey");
       // }
